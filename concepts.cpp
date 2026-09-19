cd7c70da9c881f0237f4a9e263969e6ec6227c09
Talha <talharajpootofficial@gmail.com>
2026-09-19 11:25:14 +0500

    learning and writing custom concepts and integrating them into templates..

--- concepts.cpp
+++ concepts.cpp
@@ -5,7 +5,7 @@
 
 /*
 https://www.youtube.com/watch?v=8jLOx1hD3_o&t=665s
-timestamp: 19:25:34
+timestamp: 19:42:48
 
 */
 
@@ -20,7 +20,7 @@ const T &maximum(const T &a, const T &b) requires std::integral<T> // onother wa
 
 // Base Template
 template <typename T>
-  requires std::integral<T>
+  requires std::is_integral_v<T>         // built-in concept
 const T &maximum(const T &a, const T &b) // taking refrences of arguments
 {
   return (a > b) ? a : b;
@@ -30,6 +30,47 @@ const T &maximum(const T &a, const T &b) // taking refrences of arguments
 auto add(std::floating_point auto const &a, std::floating_point auto const &b)
 {
   return a + b;
+}
+
+// custom concept
+template <typename T> // this only allows ints
+concept MyIntegral = std::is_integral_v<T>;
+
+template <typename T> // this only allows floats
+concept MyIntegral2 = std::is_floating_point_v<T>;
+
+template <typename T> // this only allows conditional
+concept MyMultiplyable = requires(T const &a, T const &b) {
+  a * b; // if this check passess (can be multiplied), it is valid.. works for both int & double
+};
+
+template <typename T> // this only allows multi conditional..
+concept MyIncrementals = requires(T a, T b) {
+  a += 1;
+  ++a;
+  a++;
+};
+
+// implementation of custom concept
+template <typename T>
+  requires MyIntegral<T>
+T multiply(T const &a, T const &b)
+{
+  return a * b;
+}
+
+template <typename T>
+  requires MyMultiplyable<T>
+T CustomAdd(T const &a, T const &b)
+{
+  return a + b;
+}
+
+template <typename T>
+  requires MyIncrementals<T>
+T CustomMultiIncr(T a, T b)
+{
+  return a * a;
 }
 
 int main()
@@ -42,6 +83,9 @@ int main()
 
   std::cout << "max(must be int): " << maximum(a, b) << std::endl;
   std::cout << "add(must be float): " << add(c, d) << std::endl;
+  std::cout << "multiply(must be integer): " << multiply(a, b) << std::endl;
+  std::cout << "CustomAdd(must fullfill condition): " << CustomAdd(a, b) << std::endl;
+  std::cout << "CustomMultiIncr(must fullfill multi condition): " << CustomMultiIncr(a, b) << std::endl;
 
   std::cout << "================================" << std::endl;
   std::cout << "Program worked successfully!" << std::endl;
